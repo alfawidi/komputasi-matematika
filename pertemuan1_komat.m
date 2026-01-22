1dee58631113d58eb5dfd6135c5f6c0322e2f32d
alfawidi <alfasetya437@gmail.com>
2026-01-22 14:58:53 +0700

    Add files via upload

---
+++ pertemuan1_komat.m
@@ -0,0 +1,5 @@
+%program luas trapesium
+a = 10; % sisi atas
+b = 14; % sisi bawah
+t = 8;  % tinggi
+luas_trapesium = 0.5 * (a + b) * t
---
+++ pertemuan2_komat.m
@@ -0,0 +1,79 @@
+% n = input ('nilai : ');
+% if n >= 70
+%     disp('anda lulus');
+% end
+% disp('coba lagi');
+
+% PERCABANGAN
+% n = input ('nilai : ');
+% if n >= 70
+%     disp('anda lulus');
+% else
+%     disp('tidak lulus');
+% end
+
+
+% ELSEIF
+% n = input ('tinggi : ');
+% if n >= 190
+%     disp('sangat tinggi');
+% elseif n > 160 && n < 190
+%     disp('cukup tinggi');
+% elseif n >= 140 && n <= 160
+%     disp('pendek');
+% else
+%     disp('mini');
+% end
+
+
+% SWITCH
+% hitungan = input ('oprasi : ');
+% a = 2;
+% b = 3;
+% switch hitungan
+%     case 1
+%         y = a+b
+%     case 2
+%         y = a-b
+%     case 3
+%         y = a*b
+%     otherwise
+%         disp('bukan pilihan');
+% end
+
+%LOOP
+% FOR_1
+% for i = 1:10
+%     disp(i);
+% end
+% FOR_2
+% a = 0;
+% for i = 1:10
+%     a = a+i;
+% end
+% disp(a)
+
+% CONTINUE
+%  a = 0;
+% for i = 1:5
+%     if i == 2
+%         continue
+%     else
+%         a = a+i;
+%     end
+% end
+% disp(a)
+
+% BREAK
+% for i = 1:10
+%     if i == 5
+%         break
+%     else
+%         disp(i);
+%     end
+% end
+
+
+
+
+
